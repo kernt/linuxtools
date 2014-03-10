6be6244e1918b3b24bf23a9b6256d1f54eb4f6a2
tobkern <tobkern1980@googlemail.com>
2014-03-10 15:34:42 +0100

    Create pre_add_client.sh

---
+++ bash/ldap/usermanagement/pre_add_client.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+#
+# Script-Name : pre_add_client.sh
+# Version : 0.1
+# Autor : Tobias Kern
+# Datum : Mo 10 Mär 2014 13:40:46 CET 
+# Lizenz : GPLv3
+# 
+# 
+#
+# Description:
+###########################################################################################
+## Schema files
+##  dhcpd.schema
+##    
+##  
+##  
+##  
+## Services:
+##    Power DNS 
+##    ISC DHCPD
+###########################################################################################
+
+DN=$1
+MACADDRESS=$2
+IP=$3
+USER=$4
+FIXADD="$MACADDRESS"
+USERNAME=$USER
+FQDN=$(hostname -f)
+DESC=$(hostname)
+
+
+echo "
+dn: $DN
+objectClass: dhcpHost
+objectClass: top
+objectClass: extensibleObject
+cn: $FQDN
+aRecord: $IP
+associatedDomain: $FQDN
+description: $DESC
+dhcpHWAddress: ethernet $MACADDRESS
+dhcpStatements: fixed-address $IP
+dNSTTL: 3600
+
+" > adduser.ldif
+
+# very impotent last empty line
+
