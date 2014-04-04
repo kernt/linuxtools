f88c95aaeb48dcd51bc3f5eed02cd3f8c17ebc78
tobkern <tobkern1980@googlemail.com>
2014-04-04 15:59:59 +0200

    Create autouseradd.sh

---
+++ bash/sysmanagement/usermanagement/autouseradd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Script-Name 			: test.sh
+# Version			      :
+# Autor				      :
+# Datum			        :
+# Lizenz			      :
+# 
+# 
+# 
+# 
+################################################################################
+
+### root only
+if [ `id -u` != 0 ]; then
+    echo "Das Script kann nur als Benutzer Root Ausgefuehrt werden!"
+    sleep 2
+    exit 1
+fi
+
+## guest user for one login
+
+
+## make tmp file for 1 login
+
+#
+
+# Description
+#
+##########################################################################################################
+#
