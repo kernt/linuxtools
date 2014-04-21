df8f1e64d79d35fa12271cbf52e0f48b559f536e
tobkern <tobkern1980@googlemail.com>
2014-04-21 04:24:33 +0200

    Create install.sh

---
+++ tmux/Vundle/install.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+#
+# Script-Name : install.sh
+# Version : 0.0.1
+# Autor : Tobias Kern
+# Datum : 18-04-2014
+# Lizenz : GPLv3
+# Depends : 
+# Use : execute it
+#
+# source        : https://github.com/gmarik/Vundle.vim
+# Description:
+
+cat  >  .vimrc <<EOF
+set nocompatible              " be iMproved, required
+filetype off                  " required
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/vundle/
+call vundle#rc()
+" alternatively, pass a path where Vundle should install plugins
+"let path = '~/some/path/here'
+"call vundle#rc(path)
+
+" let Vundle manage Vundle, required
+Plugin 'gmarik/vundle'
+
+" The following are examples of different formats supported.
+" Keep Plugin commands between here and filetype plugin indent on.
+" scripts on GitHub repos
+Plugin 'tpope/vim-fugitive'
+Plugin 'Lokaltog/vim-easymotion'
+Plugin 'tpope/vim-rails.git'
+" The sparkup vim script is in a subdirectory of this repo called vim.
+" Pass the path to set the runtimepath properly.
+Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
+" scripts from http://vim-scripts.org/vim/scripts.html
+Plugin 'L9'
+Plugin 'FuzzyFinder'
+" scripts not on GitHub
+Plugin 'git://git.wincent.com/command-t.git'
+" git repos on your local machine (i.e. when working on your own plugin)
+Plugin 'file:///home/gmarik/path/to/plugin'
+" ...
+
+filetype plugin indent on     " required
+" To ignore plugin indent changes, instead use:
+"filetype plugin on
+"
+" Brief help
+" :PluginList          - list configured plugins
+" :PluginInstall(!)    - install (update) plugins
+" :PluginSearch(!) foo - search (or refresh cache first) for foo
+" :PluginClean(!)      - confirm (or auto-approve) removal of unused plugins
+"
+" see :h vundle for more details or wiki for FAQ
+" NOTE: comments after Plugin commands are not allowed.
+" Put your stuff after this line
+EOF
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+echo "  Install Plugins:"
+echo ""
+echo "Launch vim and run :PluginInstall"
+echo ""
+echo "To install from command line: vim +PluginInstall +qall"
+
+exit 0
+
+
