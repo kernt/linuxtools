e0dd47c43cb5901545609bb16eac7db0719d3e19
tobkern <tobkern1980@googlemail.com>
2014-05-05 16:26:03 +0200

    Create getopt_de.sh

---
+++ bash/getscripts/getopt_de.sh
@@ -0,0 +1,23 @@
+# Die Option -h für Hilfe sollte immer vorhanden sein, die Optionen
+# -v und -o sind als Beispiele gedacht. -o hat ein Optionsargument;
+# man beachte das auf "o" folgende ":".
+while getopts ':o:vh' OPTION ; do
+ case $OPTION in
+ v) VERBOSE=y
+ ;;
+ o) OPTFILE="$OPTARG"
+ ;;
+ h) usage $EXIT_SUCCESS
+ ;;
+ \?) echo "Unbekannte Option \"-$OPTARG\"." >&2
+ usage $EXIT_ERROR
+ ;;
+ :) echo "Option \"-$OPTARG\" benötigt ein Argument." >&2
+ usage $EXIT_ERROR
+ ;;
+ *) echo "Dies kann eigentlich gar nicht passiert sein..."
+>&2
+ usage $EXIT_BUG
+ ;;
+ esac
+done
