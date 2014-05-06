d65a8f90811b51ab31a4908a415bc93a694119e1
tobkern <tobkern1980@googlemail.com>
2014-05-06 16:53:01 +0200

    Create addmysqluser.sh
    first idea only

---
+++ mysql/addmysqluser.sh
@@ -0,0 +1,4 @@
+# source
+# http://dev.mysql.com/doc/refman/5.6/en/create-user.html
+# http://bash.cyberciti.biz/mysql/add-database-username-password-remote-host-access/
+# Script for add user to mysql Database.
