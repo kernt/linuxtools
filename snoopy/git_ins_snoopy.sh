0cb3d1f244aec7071969134ac3ed631a780afa20
tobkern <tobkern1980@googlemail.com>
2014-06-07 01:12:29 +0200

    Update git_ins_snoopy.sh

--- snoopy/git_ins_snoopy.sh
+++ snoopy/git_ins_snoopy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
-# Script-Name : $SCRIPTNAME.sh
+# Script-Name : git_ins_snoopy.sh
 # Version : 0.01
-# Autor : $AUTUR
-# Datum : $DATE
+# Autor : Tobias Kern
+# Datum : 06.06.2014
 # Lizenz : GPLv3
 # Depends : autoconf make 
 # Use :
@@ -14,8 +14,16 @@
 ## Some Info and so one.
 ##
 ###########################################################################################
-autoconf
-./configure
-make
-make install
-make enable
+
+
+# example for tarball
+#autoconf
+#./configure
+#make
+#make install
+#make enable
+
+
+# install on Debian  Linux system
+apt-get install snoopy
+
