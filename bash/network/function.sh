4771b02d319dc31f042957960504fa7c8edc94a0
tobkern <tobkern1980@googlemail.com>
2014-09-06 23:35:02 +0200

    Create function.sh

---
+++ bash/network/function.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+function checkIPv4Address() {
+regexIP=[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}
+regexIPort=^((?:2[0-5]{2}|1\d{2}|[1-9]\d|[1-9])\.(?:(?:2[0-5]{2}|1\d{2}|[1-9]\d|\d)\.){2}(?:2[0-5]{2}|1\d{2}|[1-9]\d|\d)):(\d|[1-9]\d|[1-9]\d{2,3}|[1-5]\d{4}|6[0-4]\d{3}|654\d{2}|655[0-2]\d|6553[0-5])$
+if [  ]
+exit 1
+}
+
+function checkIPv6Address() {
+regexipv6=^((([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){6}:[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){5}:([0-9A-Fa-f]{1,4}:)?[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){4}:([0-9A-Fa-f]{1,4}:){0,2}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){3}:([0-9A-Fa-f]{1,4}:){0,3}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){2}:([0-9A-Fa-f]{1,4}:){0,4}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){6}((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|(([0-9A-Fa-f]{1,4}:){0,5}:((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|(::([0-9A-Fa-f]{1,4}:){0,5}((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|([0-9A-Fa-f]{1,4}::([0-9A-Fa-f]{1,4}:){0,5}[0-9A-Fa-f]{1,4})|(::([0-9A-Fa-f]{1,4}:){0,6}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){1,7}:))$
+
+# Muss noch Gegen die eingabe getestet werden.
+if [ $regexipv6 = "" ];
+ then
+ return 0
+else
+ return 1
+}
+
+function getMacV6IpAdd() {
+MACIPV6=$(ip -f inet6 addr show eth0 | grep "inet6" | awk '{ print $2 }')
+echo ${MACIPV6}
+}
+
+# Beispiel : getMacV6IpAdd
+
+function getMacAdd() {
+MACADD=$(ip  addr show eth0 | grep "link/ether" | awk '{print $2}')
+echo ${MACADD}
+}
+
+# Test an IP address for validity:
+# Usage:
+#      valid_ip IP_ADDRESS
+#      if [[ $? -eq 0 ]]; then echo good; else echo bad; fi
+#   OR
+#      if valid_ip IP_ADDRESS; then echo good; else echo bad; fi
+#
+function valid_ip ()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+# If run directly, execute some tests.
+if [[ "$(basename $0 .sh)" == 'valid_ip' ]]; then
+    ips='
+        4.2.2.2
+        a.b.c.d
+        192.168.1.1
+        0.0.0.0
+        255.255.255.255
+        255.255.255.256
+        192.168.0.1
+        192.168.0
+        1234.123.123.123
+        '
+    for ip in $ips
+    do
+        if valid_ip $ip; then stat='good'; else stat='bad'; fi
+        printf "%-20s: %s\n" "$ip" "$stat"
+    done
+fi
+
