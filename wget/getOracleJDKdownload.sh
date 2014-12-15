073eae37337d6490abc20f59a4a573c933b9b098
tobkern <tobkern1980@googlemail.com>
2014-12-15 09:02:37 +0100

    Create getOracleJDKdownload.sh

---
+++ wget/getOracleJDKdownload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Script-Name : $@.sh
+# Version     : 0.01
+# Autor       : Tobias Kern
+# Lizenz      : GPLv3
+# Date        : 15.12.2014
+# Depends     : 
+# Use         : 
+#
+# Example:
+#
+# Description:
+###########################################################################################
+## 
+##
+###########################################################################################
+
+# if your Cookie be defrent look in your browser for your cookie and changed gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" for own use.
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz
