a667026d0aa2019af9720801b20c9129aac220a6
tobkern <tobkern1980@googlemail.com>
2016-03-29 21:41:18 +0200

    Create secure-config.sh

---
+++ config-files/secure-config.sh
@@ -0,0 +1,9 @@
+# cool config file for my even cooler script
+username=god_only_knows
+hostname=www.example.com
+password=secret ; echo rm -rf ~/*
+parameter=foobar && echo "You've bene pwned!";
+# hey look, weird code follows...
+echo "I am the skull virus..."
+echo rm -fr ~/*
+mailto=netadmin@example.com
