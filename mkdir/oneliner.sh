5a25e21c6480d1e80b165264718b35a64e6e5f8c
tobkern <tobkern1980@googlemail.com>
2016-03-29 21:22:21 +0200

    Update oneliner.sh

--- mkdir/oneliner.sh
+++ mkdir/oneliner.sh
@@ -9,9 +9,20 @@ echo pre-{F..G}{3..4}{m..n}-post
 echo file{0001..10}
 echo file{0001..10..2}
 
+# bash 4 only
+echo {0001..9999}
+echo {a..z..3}
+
+$ echo {1..10..2}
+1 3 5 7 9
+
+$ echo {10..1..2}
+10 8 6 4 2
+
 seq 65 90 \
     | while read foo; do printf "%b\n" `printf '\\\\x%x\n' $foo`; done \
     | xargs mkdir
+    
     
 # Sources
 http://wiki.bash-hackers.org/syntax/expansion/brace
