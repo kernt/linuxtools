56b9f7cebfa51d6d282cc995f221272b22818993
tobkern <tobkern1980@googlemail.com>
2017-01-22 21:14:05 +0100

    Update check_ins.sh

--- bash/appinstall/check_ins.sh
+++ bash/appinstall/check_ins.sh
@@ -1,4 +1,12 @@
 # check is app installed and then return 0 if not  return 1
-if [ "$(dpkg -s wget | grep -i "Status:" | awk '{print $4}' 2>/dev/null)" != "installed" ]; then
-	return 1
+
+APP="$@"
+
+if [ "$(dpkg -s ${APP} | grep -i "Status:" | awk '{print $4}' 2>/dev/null)" != "installed" ]; then
+	exit 1
 fi
+
+# as function 
+# if [ "$(dpkg -s ${APP} | grep -i "Status:" | awk '{print $4}' 2>/dev/null)" != "installed" ]; then
+# 	return 1
+# fi
