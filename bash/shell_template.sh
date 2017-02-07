83e2f56d42d721d76bb7f0c85bec360bed391583
tobkern <tobkern1980@googlemail.com>
2017-02-07 15:34:58 +0100

    Update shell_template.sh

--- bash/shell_template.sh
+++ bash/shell_template.sh
@@ -49,6 +49,17 @@ while getopts ':o:vh' OPTION ; do
 done
 # Verbrauchte Argumente überspringen
 shift $(( OPTIND - 1 ))
+
+# Farben für die shell
+black() { echo "$(tput setaf 0)$*$(tput setaf 9)"; }
+red() { echo "$(tput setaf 1)$*$(tput setaf 9)"; }
+green() { echo "$(tput setaf 2)$*$(tput setaf 9)"; }
+yellow() { echo "$(tput setaf 3)$*$(tput setaf 9)"; }
+blue() { echo "$(tput setaf 4)$*$(tput setaf 9)"; }
+magenta() { echo "$(tput setaf 5)$*$(tput setaf 9)"; }
+cyan() { echo "$(tput setaf 6)$*$(tput setaf 9)"; }
+white() { echo "$(tput setaf 7)$*$(tput setaf 9)"; }
+
 # Eventuelle Tests auf min./max. Anzahl Argumente hier
 if (( $# < 1 )) ; then
  echo "Mindestens ein Argument beim Aufruf übergeben." >&2
